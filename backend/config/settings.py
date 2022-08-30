79e678e2ea70a827d58abe5c5ab71852ac47a1e0
Numan Ibn Mazid <numanibnmazid@gmail.com>
2022-08-31 00:39:08 +0600

    github action updated

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -90,13 +90,13 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
-	},
+    },
     {
         "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
-	},
+    },
     {
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
-	},
+    },
 ]
 
 

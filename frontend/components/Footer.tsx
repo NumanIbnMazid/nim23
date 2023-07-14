561f73ae137ca349085ce4ec664202f97be42d16
Numan Ibn Mazid <numanibnmazid@gmail.com>
2023-07-15 04:10:36 +0600

    Icon Added
    - Spotify removed
    - Icon and Logo Added

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -1,29 +1,25 @@
-import Link from "next/link";
-import Image from "next/image";
-import socialMedia from "@content/socialMedia";
+import Link from "next/link"
+import socialMedia from "@content/socialMedia"
 import {
   FadeContainer,
   opacityVariant,
   popUp,
-} from "../content/FramerMotionVariants";
-import { navigationRoutes } from "../utils/utils";
-import { motion } from "framer-motion";
-import { SiSpotify } from "react-icons/si";
-import useSWR from "swr";
-import fetcher from "../lib/fetcher";
-import { HiOutlineQrcode } from "react-icons/hi";
-import { BsDot } from "react-icons/bs";
-import { Song } from "@lib/types";
+} from "../content/FramerMotionVariants"
+import { navigationRoutes } from "../utils/utils"
+import { motion } from "framer-motion"
+import useSWR from "swr"
+import fetcher from "../lib/fetcher"
+import { HiOutlineQrcode } from "react-icons/hi"
+import { BsDot } from "react-icons/bs"
 
 export default function Footer({
   setShowQR,
   showQR,
 }: {
-  setShowQR: (value: boolean) => void;
-  showQR: boolean;
+  setShowQR: (value: boolean) => void
+  showQR: boolean
 }) {
-  const { data: currentSong } = useSWR("/api/now-playing", fetcher);
-  const { data: visitors } = useSWR("/api/ga", fetcher);
+  const { data: visitors } = useSWR("/api/ga", fetcher)
 
   return (
     <footer className="w-screen text-gray-600 dark:text-gray-400/50 font-inter mb-14 print:hidden">
@@ -34,15 +30,6 @@ export default function Footer({
         viewport={{ once: true }}
         className="flex flex-col max-w-4xl gap-5 p-5 mx-auto text-sm border-t-2 border-gray-200 2xl:max-w-5xl 3xl:max-w-7xl dark:border-gray-400/10 sm:text-base"
       >
-        {/* Spotify currently playing */}
-        <div>
-          {currentSong?.isPlaying ? (
-            <WhenPlaying song={currentSong} />
-          ) : (
-            <NotPlaying />
-          )}
-        </div>
-
         <section className="grid grid-cols-3 gap-10">
           {/* 1st 5 navigation routes */}
           <div className="flex flex-col gap-4 capitalize">
@@ -123,7 +110,7 @@ export default function Footer({
         </motion.div>
       </motion.div>
     </footer>
-  );
+  )
 }
 
 function FooterLink({ route, text }: { route: string; text: string }) {
@@ -136,59 +123,5 @@ function FooterLink({ route, text }: { route: string; text: string }) {
         {text}
       </motion.p>
     </Link>
-  );
-}
-
-function NotPlaying() {
-  return (
-    <div className="flex flex-row-reverse items-center justify-between gap-2 sm:flex-row sm:justify-start">
-      <SiSpotify className="w-6 h-6" />
-      <div className="flex flex-col sm:flex-row sm:items-center sm:gap-3">
-        <div className="font-semibold text-black md:text-lg dark:text-white">
-          Not Playing
-        </div>
-        <span className="hidden md:inline-flex">—</span>
-        <p className="text-xs text-gray-500 sm:text-sm">Spotify</p>
-      </div>
-    </div>
-  );
-}
-
-function WhenPlaying({ song }: { song: Song }) {
-  return (
-    <div className="flex flex-col gap-4">
-      <h4 className="text-lg font-semibold dark:text-gray-300">Now Playing</h4>
-      <Link
-        href={song.songUrl}
-        className="flex items-center justify-between p-3 bg-gray-200 rounded-sm dark:bg-darkSecondary sm:p-4"
-      >
-        <div className="flex items-center gap-2 ">
-          <div className="w-10 h-10">
-            <Image
-              alt={song.title}
-              src={song.albumImageUrl}
-              width={40}
-              height={40}
-              quality={50}
-              placeholder="blur"
-              blurDataURL={song.albumImageUrl}
-            />
-          </div>
-          <div className="flex flex-col sm:flex-row sm:items-center sm:gap-3">
-            <h3 className="font-semibold text-black md:text-lg dark:text-white animate-">
-              {song.title}
-            </h3>
-            <span className="hidden md:inline-flex dark:text-gray-300">—</span>
-
-            <p className="text-xs text-gray-600 dark:text-gray-400 sm:text-sm">
-              {song.artist}
-            </p>
-          </div>
-        </div>
-        <div className="flex items-center gap-2">
-          <SiSpotify className="w-6 h-6 text-green-500 animate-[spin_2s_linear_infinite]" />
-        </div>
-      </Link>
-    </div>
-  );
+  )
 }

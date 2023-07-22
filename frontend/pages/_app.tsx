a6d819614e153ece3404166eba3ae33749440d14
Numan Ibn Mazid <numanibnmazid@gmail.com>
2023-07-22 15:30:56 +0600

    service worker updated

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -35,6 +35,21 @@ function MyApp({ Component, pageProps }: AppProps) {
     }
   }, [router.events])
 
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', () => {
+        navigator.serviceWorker
+          .register('/sw.js')
+          .then((registration) => {
+            console.log('Service Worker registered:', registration)
+          })
+          .catch((error) => {
+            console.error('Service Worker registration failed:', error)
+          })
+      })
+    }
+  }, [])
+
   return (
     <DarkModeProvider>
       <Layout>

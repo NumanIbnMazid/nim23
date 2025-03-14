42fe835b4cd90c54c4a9e48782c2e2164ac2b2c7
Numan Ibn Mazid <numanibnmazid@gmail.com>
2025-03-14 22:20:12 +0600

    Northflank domain added

--- backend/project/settings/production.py
+++ backend/project/settings/production.py
@@ -5,10 +5,9 @@ from project.settings.base import *
 # ----------------------------------------------------
 ALLOWED_HOSTS = [
     "nim23.com",  # Frontend Host
+    "nim23.backend.com",  # Backend Host
     "nim23.koyeb.app",  # Backend Host
-    "nim23.onrender.com",
-    "nursing-violante-nim23-c4da3b73.koyeb.app",
-    "numanibnmazid.pythonanywhere.com"
+    "p01--nim23--47v76khxj6jn.code.run",  # Northflank Host
 ]
 
 # ----------------------------------------------------

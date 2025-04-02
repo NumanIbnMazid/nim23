ac62528b15547f18c148ac7a9a3190e45e69e1b0
Numan Ibn Mazid <numanibnmazid@gmail.com>
2025-04-02 07:06:55 +0600

    max duration updated

--- frontend/src/app/grabit/page.tsx
+++ frontend/src/app/grabit/page.tsx
@@ -2,6 +2,8 @@ import GrabitClient from "@/app/grabit/GrabitClient";
 
 export const dynamic = 'force-dynamic'
 
+export const maxDuration = 60
+
 export default function Page() {
     return <GrabitClient />;
 }

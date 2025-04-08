b0b6b659b1aa4e06a16031674c4c2a60dba6f155
Numan Ibn Mazid <numanibnmazid@gmail.com>
2025-04-08 23:06:20 +0600

    Layout removed

--- frontend/src/app/apps/page.tsx
+++ frontend/src/app/apps/page.tsx
@@ -1,10 +1,37 @@
-// src/app/apps/page.tsx
-import AppsPage from '@/app/apps/AppsPage'
+import { Suspense } from 'react'
+import SkeletonLoader from '@/components/SkeletonLoader'
+import { getPageMetadata, pageMeta } from '@/lib/Meta'
+import { Metadata } from 'next'
+import { PUBLIC_SITE_URL } from '@/lib/constants'
+import AppsClient from '@/app/apps/AppsPageClient'
+
+// Revalidate every 60 seconds
+export const revalidate = 60
+
+// Generate metadata for the landing page
+export const metadata: Metadata = getPageMetadata({
+  title: pageMeta.home.title,
+  description: pageMeta.home.description,
+  image: pageMeta.home.image,
+  keywords: pageMeta.home.keywords,
+  url: PUBLIC_SITE_URL,
+})
 
 export default function Page() {
   return (
-    <div>
-      <AppsPage />
-    </div>
+    <Suspense fallback={<SkeletonLoader />}>
+      <AppsLandingPage />
+    </Suspense>
   )
+}
+
+async function AppsLandingPage() {
+  const apps = [
+    {
+      name: 'Grabit',
+      link: '/apps/grabit',
+      description: "Online Video Downloader and Audio Converter"
+    },
+  ]
+  return <AppsClient apps={apps} />
 }

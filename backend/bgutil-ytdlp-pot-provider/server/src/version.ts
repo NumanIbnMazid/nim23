d757738f26c9c88f0eb6081f1c87feaefff8feb7
Numan Ibn Mazid <numanibnmazid@gmail.com>
2025-04-08 20:40:03 +0600

    bg-util removed

--- backend/bgutil-ytdlp-pot-provider/server/src/version.ts
+++
@@ -1,8 +0,0 @@
-import * as fs from "fs";
-import * as path from "path";
-
-const packageJson = fs.readFileSync(
-    path.resolve(__dirname, "..", "package.json"),
-    "utf8",
-);
-export const VERSION = JSON.parse(packageJson).version;

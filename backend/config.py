7158748e8b0e342e0496195f398d4a7301337c1d
Numan Ibn Mazid <numanibnmazid@gmail.com>
2025-04-13 17:07:25 +0600

    Pydantic version updated 🔥

--- backend/config.py
+++ backend/config.py
@@ -1,44 +1,43 @@
 from typing import Any
-from pydantic import BaseSettings, Field
+from pydantic import Field
+from pydantic_settings import BaseSettings
 from pathlib import Path
 
 
 class BaseConfig(BaseSettings):
-    MODE: str = Field(..., regex="(DEVELOPMENT|STAGING|PRODUCTION)")
-    LOG_LEVEL: str = Field("DEBUG", regex="(DEBUG|INFO|WARNING|ERROR|CRITICAL)")
-    SECRET_KEY: str = Field(..., env="SECRET_KEY")
-    DJANGO_LOG_LEVEL: str = Field("INFO", env="DJANGO_LOG_LEVEL")
-    CLOUDINARY_CLOUD_NAME: str = Field(..., env="CLOUDINARY_CLOUD_NAME")
-    CLOUDINARY_API_KEY: str = Field(..., env="CLOUDINARY_API_KEY")
-    CLOUDINARY_API_SECRET: str = Field(..., env="CLOUDINARY_API_SECRET")
-    BACKEND_BASE_URL: str = Field(..., env="BACKEND_BASE_URL")
+    MODE: str = Field(..., pattern="(DEVELOPMENT|STAGING|PRODUCTION)")
+    LOG_LEVEL: str = Field("DEBUG", pattern="(DEBUG|INFO|WARNING|ERROR|CRITICAL)")
+    SECRET_KEY: str = Field(..., alias="SECRET_KEY")
+    DJANGO_LOG_LEVEL: str = Field("INFO", alias="DJANGO_LOG_LEVEL")
+    CLOUDINARY_CLOUD_NAME: str = Field(..., alias="CLOUDINARY_CLOUD_NAME")
+    CLOUDINARY_API_KEY: str = Field(..., alias="CLOUDINARY_API_KEY")
+    CLOUDINARY_API_SECRET: str = Field(..., alias="CLOUDINARY_API_SECRET")
+    BACKEND_BASE_URL: str = Field(..., alias="BACKEND_BASE_URL")
 
     class Config:
         env_file = f"{Path(__file__).resolve().parent.parent}/.env"
+        extra = "allow"
 
 
 class DatabaseConfig(BaseConfig):
-    NAME: str = Field("numanibnmazid_portfolio", env="DATABASE_NAME")
-    USER: str = Field("numanibnmazid", env="DATABASE_USER")
-    HOST: str = Field(..., env="DATABASE_HOST")
-    PORT: int = Field(5432, env="DATABASE_PORT")
-    PASSWORD: str = Field(..., env="DATABASE_PASSWORD")
+    NAME: str = Field("numanibnmazid_portfolio", alias="DATABASE_NAME")
+    USER: str = Field("numanibnmazid", alias="DATABASE_USER")
+    HOST: str = Field(..., alias="DATABASE_HOST")
+    PORT: int = Field(5432, alias="DATABASE_PORT")
+    PASSWORD: str = Field(..., alias="DATABASE_PASSWORD")
 
 
 class ProjectConfig(BaseConfig):
-    DATABASE = DatabaseConfig()
+    DATABASE: BaseConfig = DatabaseConfig()
 
 
-class ProjectDevelopmentConfig(ProjectConfig):
-    ...
+class ProjectDevelopmentConfig(ProjectConfig): ...
 
 
-class ProjectStagingConfig(ProjectConfig):
-    ...
+class ProjectStagingConfig(ProjectConfig): ...
 
 
-class ProjectProductionConfig(ProjectConfig):
-    ...
+class ProjectProductionConfig(ProjectConfig): ...
 
 
 class ConfigFactory:
